6bbd29e09ff77e240135e72654f1edb7d7f2bbfe
sirkays <attehkayode2@gmail.com>
2026-06-30 12:59:02 +0100

    Disable next/image optimization to fix CMS image frontend 404s and save server RAM

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {protocol: "https", hostname: "lh3.googleusercontent.com"},
       {protocol: "https", hostname: "images.unsplash.com"},
